7c4b131a42de8d2321d0e360e787d208886fa5e1
alsdl0629 <ehfehf0629@gmail.com>
2023-10-11 15:39:11 +0900

    Setting: 프로젝트 세팅

--- src/main/java/meister/hackaton/cashopserver/CashopserverApplication.java
+++ src/main/java/meister/hackaton/cashopserver/CashopserverApplication.java
@@ -2,7 +2,9 @@ package meister.hackaton.cashopserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
 @SpringBootApplication
 public class CashopserverApplication {
 
